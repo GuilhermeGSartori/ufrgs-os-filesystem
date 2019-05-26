51a76c6e6f351dae2fc08a68ffb06ee6b8f8a38c
Renan Kummer <35353992+RenanKummer@users.noreply.github.com>
2019-05-25 21:26:16 -0300

    Add constants.h

---
+++ include/constants.h
@@ -0,0 +1,16 @@
+#ifndef UFRGS_OS_FILESYSTEM_INCLUDE_CONSTANTS_H
+#define UFRGS_OS_FILESYSTEM_INCLUDE_CONSTANTS_H
+/**
+ * Definition of constants to be reused in the library.
+ * 
+ * @author Renan Kummer
+ */
+
+typedef enum ReturnCode
+{
+    T2FS_SUCCESS =  0,
+    T2FS_FAILURE = -1
+} ReturnCode;
+
+
+#endif // UFRGS_OS_FILESYSTEM_INCLUDE_CONSTANTS_H

c5d0e7f71aef87fa65ba2c0310baa57179d85d53
Renan Kummer <dev.renankummer@outlook.com>
2019-06-16 14:42:22 -0300

    Declare prototypes for new support functions
    In support.h:
      - read_block() to retrieve the content of a block from disk.
      - parse_path() to break a path into array of entries.
      - get_entry_name() to obtain the name of an entry from a path.
      - calculate_hash_table_index() to obtain the index for an entry.
    In constants.h:
      - Add error codes in ReturnCode enum.

--- include/constants.h
+++ include/constants.h
@@ -3,6 +3,8 @@
 /**
  * Definition of constants to be reused in the library.
  * 
+ * @author Guilherme Sartori
+ * @author Marlize Ramos
  * @author Renan Kummer
  */
 
@@ -11,8 +13,11 @@ typedef enum ReturnCode
     T2FS_SUCCESS =  0,
     T2FS_FAILURE = -1,
 
-    T2FS_NULL_POINTER      = -2,
-    T2FS_NOT_ENOUGH_MEMORY = -3
+    T2FS_NULL_POINTER         = -2,
+    T2FS_NOT_ENOUGH_MEMORY    = -3,
+    T2FS_NO_FREE_HANDLE       = -4,
+    T2FS_DIRECTORY_IS_FULL    = -5,
+    T2FS_INVALID_BLOCK_NUMBER = -6
 } ReturnCode;
 
 
